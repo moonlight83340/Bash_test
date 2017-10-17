d7538cdec2b45f92cf1bc677e9210f67dd08a46d
moonlight83340 <moonlight83340@users.noreply.github.com>
2017-10-17 09:11:45 +0200

    Add files via upload

---
+++ getScan.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Author Gaëtan
+
+#main
+
+usage(){
+	echo "Usage: ./getScan.sh <name-of-manga> [target-dir]";
+	exit 1;
+}
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "usage" ];then
+	usage
+	exit 0
+fi
+
+if [ ! $# -lt 2 ]; then
+	usage
+fi
+#first use jpscanlib
+. jpscanlib.sh
+getChapterCount "$1"
+chapter=$?
+echo $chapter
+if [ $chapter -eq 0 ];then
+	#if jpscan have no result we try on manga-reader
+	. manga-readerlib.sh
+	URL="http://www.mangareader.net/${1}"
+	getChapterCount $URL $1
+	chapter=$?
+	echo $chapter
+fi
+for (( chap=1; chap<=$chapter; chap++ )); do
+	get "$1" $chap "$2"
+done;
+exit 0
+
